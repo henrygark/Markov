4328371fc6b923ba6f849c4276b94adf87c844ee
Henry Garkanian <hgarkanian@csumb.edu>
2023-11-11 16:06:47 -0800

    Was able to get randomWords, addWords, getWords to pass. Continuing.

--- src/main/java/org/example/Markov.java
+++ src/main/java/org/example/Markov.java
@@ -40,42 +40,59 @@ public class Markov {
             addLine(collect);
         }
     }
-    public void addLine(String line) {
-        line  = String.valueOf(words.get(BEGINS_SENTENCE_));
-        if (line == null) {
-            System.out.println("");
-        }
-        if (line != null) {
-        line.split("//");
+    public void addLine(String line) {      //tool to get words out of sentence and put them into an arraylist to access.
+        if (line.length() != 0 ) {
+            String[] myWords = line.split(" ");   // found from https://www.javatpoint.com/substring
+
+            for (int i = 0; i < myWords.length; i++) {
+                if (myWords[i].length() != 0) {
+                    addWord(myWords[i]);      //retrieves string and adds the value from the words array.
+                                             //words[i] retrieves the value from the words array.
+                }
+            }
+            if (line != null) {
+                line.split(" ");
+            }
         }
     }
     public void addWord(String word) {
-        System.out.println("Word not implemented");
+        if (endsWithPunctuation(prevWord)) {
+            ArrayList<String> array = new ArrayList<String>();
+            array.add(word);
+
+            words.put(BEGINS_SENTENCE_, array);
+        } else if (!endsWithPunctuation(prevWord)){
+            if (!words.containsKey(prevWord)) {
+                ArrayList<String> array = new ArrayList<String>();
+                array.add(word);
+                words.put(prevWord, array);
+            } else {
+                ArrayList<String> array = words.get(prevWord);
+                array.add(word);
+                words.put(prevWord, new ArrayList<>());
+            }
+        }
+        prevWord = word;
     }
     public static boolean endsWithPunctuation(String word) {
-       return PUNCTUATION_MARKS_.contains(word.substring(word.length() - 2));
+       return PUNCTUATION_MARKS_.contains(word.substring(word.length() - 1));
        }
 
     public String randomWord(String key) {
         Random wordGen = new Random();
-        Object[] values = words.values().toArray();         //found from https://stackoverflow.com/questions/929554/is-there-a-way-to-get-the-value-of-a-hashmap-randomly-in-java
-        Object randomWord = values[wordGen.nextInt(values.length)];
+        ArrayList<String> initial = words.get(key);    //retrieves the object
 
-
-    System.out.println("Random word not implemented");
-        return key;
+        return initial.get(wordGen.nextInt(initial.size()));
     }
     @Override
-    public String toString()
-    {
+    public String toString() {
         return words.toString();
     }
     public String getSentence() {
         String currentWord = "";
         currentWord = randomWord(BEGINS_SENTENCE_);
-      //  if (currentWord.contains(endsWithPunctuation())) {
-
-       // }
+        //  if (currentWord.contains(endsWithPunctuation())) {
+        // }
 
         System.out.println("Not implemented");
         return "";
